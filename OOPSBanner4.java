64145a056336f9f5c67b4a22c6f2aaa2a0524719
Pixelator239 <pixelator239@gmail.com>
2026-03-10 21:01:14 +0530

    feature(UC4): added OOPSBanner4 class

---
+++ OOPSBanner4.java
@@ -0,0 +1,26 @@
+/**
+ * OOPS Banner App
+ * UC4 - Print OOPS Banner with help of arrays
+ * @author Srinidh
+ * @version .0
+ */
+
+public class OOPSBanner4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = new String[7];
+
+        banner[0] = String.join("   ", " *****  ", " *****  ", " ****** ", "  ***** ");
+        banner[1] = String.join("   ", "*     * ", "*     * ", "*     * ", " *     * ");
+        banner[2] = String.join("   ", "*     * ", "*     * ", "*     * ", " *     ");
+        banner[3] = String.join("   ", "*     * ", "*     * ", " ****** ", " *****  ");
+        banner[4] = String.join("   ", "*     * ", "*     * ", "*       ", "      * ");
+        banner[5] = String.join("   ", "*     * ", "*     * ", "*       ", "*     * ");
+        banner[6] = String.join("   ", " *****  ", " *****  ", "*       ", " *****  ");
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
