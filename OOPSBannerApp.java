762b9e18cec9bcbdcf12f5b95492ec14bbba13cd
Pixelator239 <pixelator239@gmail.com>
2026-03-21 13:42:49 +0530

    feature/UC8: added OOPSBanner8.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,76 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * OOPS Banner App
+ * UC8 - Use Map for Character Patterns and Render via Function
+ * @author Srinidh
+ * @version 8.0
+ */
+
+public class OOPSBannerApp {
+
+    // Utility Method 1: Create and return the Map
+    public static Map<Character, String[]> buildCharacterMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " *****  ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                " *****  "
+        });
+
+        map.put('P', new String[]{
+                " ****** ",
+                "*     * ",
+                "*     * ",
+                " ****** ",
+                "*        ",
+                "*        ",
+                "*        "
+        });
+
+        map.put('S', new String[]{
+                "  *****  ",
+                " *     * ",
+                " *        ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        });
+
+        return map;
+    }
+
+    // Utility Method 2: Render Banner
+    public static void renderBanner(String message, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+                String[] pattern = map.get(ch);
+                sb.append(pattern[row]).append("   ");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+
+        Map<Character, String[]> characterMap = buildCharacterMap();
+
+        String message = "OOPS";
+
+        renderBanner(message, characterMap);
+    }
+}
