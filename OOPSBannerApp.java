6d8ad94e9e103a173495b47a28c9ad772b29817a
Pixelator239 <pixelator239@gmail.com>
2026-03-21 12:15:59 +0530

    feature(UC5): added OOPSBanner5

---
+++ OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/**
+ * OOPS Banner App
+ * UC5 - Render OOPS as Banner using Inline Array Initialization
+ * @author Srinidh
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("   ", " *****  ", " *****  ", " ****** ", "  ***** "),
+            String.join("   ", "*     * ", "*     * ", "*     * ", " *     * "),
+            String.join("   ", "*     * ", "*     * ", "*     * ", " *       "),
+            String.join("   ", "*     * ", " *****  ", " ****** ", "  ***** "),
+            String.join("   ", "*     * ", "*       ", "*      ", "       * "),
+            String.join("   ", "*     * ", "*       ", "*      ", " *     * "),
+            String.join("   ", " *****  ", "*       ", "*      ", "  *****  ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
